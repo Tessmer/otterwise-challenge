886bb009428bb66bfc44dfbcbacacdf9dc5cf38c
Otterwise <81589726+otterwise@users.noreply.github.com>
2021-03-29 17:12:29 -0300

    Add files via upload

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+import "../styles/style.css";
+
+const Form = ({ onSubmit }) => {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <form className="container" onSubmit={handleSubmit(onSubmit)}>
+      <input ref={register} name="title" placeholder="Título" />
+      <input ref={register} placeholder="Descrição" />
+      <div className="m-top-25">
+        <button type="submit" className="info">
+          Criar
+        </button>
+        <Link to="/">
+          <button type="button" className="cancel">
+            Cancelar
+          </button>
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default Form;
